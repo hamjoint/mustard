4f4bf002ce55e72d378fd40f287d0b0b2aaab314
Tom Pitcher <tom@hamjoint.org>
2015-09-02 21:10:21 +0100

    Added fix for camel_case relations to Model abstract class

--- src/lib/Model.php
+++ src/lib/Model.php
@@ -31,7 +31,8 @@ abstract class Model extends \Illuminate\Database\Eloquent\Model
     public $timestamps = false;
 
     /**
-     * Mustard uses camel case for class properties, and snake case for database columns.
+     * Mustard uses camel case for class properties, and snake case for
+     * database columns.
      *
      * @param string $property
      * @return mixed
@@ -39,6 +40,32 @@ abstract class Model extends \Illuminate\Database\Eloquent\Model
     public function __get($property)
     {
         return parent::__get(snake_case($property));
+    }
+
+    /**
+     * Get a relationship. We override this method from the parent to add a
+     * camel_case() conversion for the key.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function getRelationValue($key)
+    {
+        $key = camel_case($key);
+
+        // If the key already exists in the relationships array, it just means the
+        // relationship has already been loaded, so we'll just return it out of
+        // here because there is no need to query within the relations twice.
+        if ($this->relationLoaded($key)) {
+            return $this->relations[$key];
+        }
+
+        // If the "attribute" exists as a method on the model, we will just assume
+        // it is a relationship and will load and return results from the query
+        // and hydrate the relationship's value on the "relationships" array.
+        if (method_exists($this, $key)) {
+            return $this->getRelationshipFromMethod($key);
+        }
     }
 
     /**
