6556d3a6a78b06ad6f9750817c1da8010994ff21
Tom Pitcher <tom@hamjoint.org>
2015-10-22 16:35:18 +0100

    Added routes and service provider

---
+++ src/includes/routes.php
@@ -0,0 +1,53 @@
+<?php
+
+Route::group([
+    'prefix' => env('MUSTARD_BASE', ''),
+    'namespace' => 'Hamjoint\Mustard\Http\Controllers',
+], function()
+{
+    Route::group([
+        'middleware' => 'auth',
+    ], function()
+    {
+        Route::get('account', 'AccountController@getIndex');
+        Route::get('account/password', 'AccountController@getPassword');
+        Route::post('account/password', 'AccountController@postPassword');
+        Route::get('account/email', 'AccountController@getEmail');
+        Route::post('account/email', 'AccountController@postEmail');
+        Route::get('account/contacts', 'AccountController@getContacts');
+        Route::get('account/notifications', 'AccountController@getNotifications');
+        Route::post('account/notifications', 'AccountController@postNotifications');
+        Route::get('account/close', 'AccountController@getClose');
+
+        Route::get('inventory', ['uses' => 'InventoryController@getIndex']);
+        Route::get('inventory/watching', ['uses' => 'InventoryController@getWatching']);
+
+        Route::get('sell', 'ItemController@getNew');
+        Route::get('item/new', 'ItemController@getNew');
+        Route::post('item/new', 'ItemController@postNew');
+        Route::get('item/edit', 'ItemController@getEdit');
+        Route::get('item/relist', 'ItemController@getRelist');
+        Route::get('item/end', 'ItemController@getEnd');
+        Route::post('item/end', 'ItemController@postEnd');
+        Route::post('item/cancel', 'ItemController@postCancel');
+        Route::post('item/watch', 'ItemController@postWatch');
+        Route::post('item/unwatch', 'ItemController@postUnwatch');
+
+        Route::controller('admin', 'AdminController');
+    });
+
+    Route::get('buy', ['uses' => 'ListingController@getIndex']);
+    Route::get('buy/{categories}', ['uses' => 'ListingController@getIndex'])->where('categories', '(.+)');
+
+    Route::get('item/{id}/{slug?}', ['uses' => 'ItemController@getIndex'])->where('id', '[0-9]+');
+
+    Route::get('user/{id}', ['uses' => 'UserController@getIndex']);
+
+    Route::get('glossary/{page}', ['uses' => 'GlossaryController@getView'])->where('page', '[a-z0-9-]+');
+    Route::controller('glossary', 'GlossaryController');
+
+    Route::get('help/{page}', ['uses' => 'HelpController@getView'])->where('page', '[a-z0-9-]+');
+    Route::controller('help', 'HelpController');
+
+    Route::get('', 'MetaController@getIndex');
+});
---
+++ src/lib/Providers/MustardServiceProvider.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+
+This file is part of Mustard.
+
+Mustard is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Mustard is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Mustard.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+namespace Hamjoint\Mustard\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MustardServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Include routes
+        if (!$this->app->routesAreCached()) {
+            require __DIR__ . '/../../includes/routes.php';
+        }
+
+        // Load views
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'mustard');
+
+        // Publish migrations
+        $this->publishes([
+            __DIR__ . '/../../database/migrations/' => database_path('migrations')
+        ], 'migrations');
+
+        // Require helpers
+        require __DIR__ . '/../../includes/helpers.php';
+
+        // Require validators
+        require __DIR__ . '/../../includes/validators.php';
+    }
+
+    /**
+     * Register any services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
