3d811d4cfb705a6e2106db5083db07c3b7899eff
Tom Pitcher <tom@hamjoint.org>
2015-10-22 16:55:01 +0100

    Added Admin and Account controllers

---
+++ src/lib/Http/Controllers/AccountController.php
@@ -0,0 +1,198 @@
+<?php
+
+/*
+
+This file is part of Mustard.
+
+Mustard is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Mustard is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Mustard.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+namespace Hamjoint\Mustard\Http\Controllers;
+
+use Auth;
+use Hamjoint\Mustard\User;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator as Paginator;
+
+class AccountController extends Controller
+{
+    /**
+     * Redirect index requests to the password page.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function getIndex()
+    {
+        return redirect('/account/password');
+    }
+
+    /**
+     * Return account password view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function getPassword()
+    {
+        return view('mustard::account.password', [
+            'page' => "password",
+        ]);
+    }
+
+    /**
+     * Change account password.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function postPassword(Request $request)
+    {
+        $this->validates(
+            $request->all(),
+            [
+                'old_password' => 'required',
+                'new_password' => 'required|min:8',
+            ]
+        );
+
+        if (!\Hash::check($request->get('old_password'), Auth::user()->password_hash)) {
+            return redirect('/account/password')
+                ->withErrors(['old_password' => "Your old password is not correct."]);
+        }
+
+        if ($request->get('old_password') == $request->get('new_password')) {
+            return redirect('/account/password');
+        }
+
+        Auth::user()->passwordHash = \Hash::make($request->get('new_password'));
+
+        Auth::user()->save();
+
+        Auth::user()->sendEmail(
+            'Your password has been changed',
+            'emails.account.password-changed'
+        );
+
+        return redirect()->back()
+            ->with('message', 'Your password has been changed.');
+    }
+
+    /**
+     * Return account email view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function getEmail()
+    {
+        return view('mustard::account.email', [
+            'page' => "email",
+        ]);
+    }
+
+    /**
+     * Change account email address.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function postEmail(Request $request)
+    {
+        $this->validates(
+            $request->all(),
+            [
+                'email' => 'required|email',
+            ]
+        );
+
+        if ($request->get('email') == Auth::user()->email) {
+            return redirect('/account/email');
+        }
+
+        if (User::findByEmail($request->get('email'))) {
+            self::formFlash(['email']);
+
+            return redirect('/account/email')
+                ->withErrors(['email' => "That email address is in use by another account."]);
+        }
+
+        Auth::user()->sendEmail(
+            'Your email address has been changed',
+            'emails.account.email-changed',
+            ['new_email' => $request->get('email')]
+        );
+
+        Auth::user()->email = $request->get('email');
+
+        Auth::user()->sendEmail(
+            'Verify your new email address',
+            'emails.account.verify',
+            [
+                'key' => Auth::user()->addVerification(),
+                'new_account' => false,
+            ]
+        );
+
+        Auth::user()->verified = false;
+
+        Auth::user()->save();
+
+        return redirect()->back()
+            ->with('message', 'Your email address has been changed.');
+    }
+
+    /**
+     * Return account contacts view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function getContacts(Request $request)
+    {
+        $acquaintances = Auth::user()->getAcquaintances();
+
+        return view('mustard::account.contacts', [
+            'acquaintances' => new Paginator($acquaintances, $acquaintances->count(), config('per_page', 25)),
+        ]);
+    }
+
+    /**
+     * Return account notifications view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function getNotifications()
+    {
+        return view('mustard::account.notifications', [
+            'page' => "notifications",
+        ]);
+    }
+
+    /**
+     * Change account notification settings.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     * @todo Add logic
+     */
+    public function postNotifications()
+    {
+        return redirect()->back();
+    }
+
+    /**
+     * Return account close view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function getClose()
+    {
+        return view('mustard::account.close');
+    }
+}
---
+++ src/lib/Http/Controllers/AdminController.php
@@ -0,0 +1,190 @@
+<?php
+
+/*
+
+This file is part of Mustard.
+
+Mustard is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Mustard is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Mustard.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+namespace Hamjoint\Mustard\Http\Controllers;
+
+use Cache;
+use Hamjoint\Mustard\Item;
+use Hamjoint\Mustard\Tables\AdminItems;
+use Hamjoint\Mustard\Tables\AdminUsers;
+use Hamjoint\Mustard\User;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * Redirect index requests to the dashboard page.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function getIndex()
+    {
+        return redirect('/admin/dashboard');
+    }
+
+    /**
+     * Return the admin dashboard view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function getDashboard()
+    {
+        $stats = [
+            'User stats' => [
+                'Registered' => function($range)
+                {
+                    return n(User::totalRegistered($range));
+                },
+                'Bidders' => function($range)
+                {
+                    return n(User::totalBidders($range));
+                },
+                'Buyers' => function($range)
+                {
+                    return n(User::totalBuyers($range));
+                },
+                'Sellers' => function($range)
+                {
+                    return n(User::totalSellers($range));
+                },
+            ],
+            'Item stats' => [
+                'Listed' => function($range)
+                {
+                    return n(Item::totalListed($range));
+                },
+                'Bids placed' => function($range)
+                {
+                    return n(\Hamjoint\Mustard\Auctions\Bid::totalPlaced($range));
+                },
+                'Average bid amount' => function($range)
+                {
+                    return p(\Hamjoint\Mustard\Auctions\Bid::averageAmount($range));
+                },
+                'Watched' => function($range)
+                {
+                    return n(Item::totalWatched($range));
+                },
+            ],
+            'Transaction stats' => [
+                'Purchases' => function($range)
+                {
+                    return n(\Hamjoint\Mustard\Commerce\Purchase::totalCreated($range));
+                },
+                'Average amount' => function($range)
+                {
+                    return p(\Hamjoint\Mustard\Commerce\Purchase::averageAmount($range));
+                },
+            ],
+        ];
+
+        $ranges = [
+            'Today' => strtotime('midnight'),
+            'This week' => strtotime('monday this week'),
+            'This month' => strtotime('midnight first day of this month'),
+            'This year' => strtotime(date('Y') . '/01/01'),
+            'Overall' => 0,
+        ];
+
+        return view('mustard::admin.dashboard', [
+            'ranges' => $ranges,
+            'stats' => $stats,
+        ]);
+    }
+
+    /**
+     * Return the admin items view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function getItems(Request $request)
+    {
+        $table = new AdminItems(Item::query(), $request);
+
+        return view('mustard::admin.items', [
+            'table' => $table,
+        ]);
+    }
+
+    /**
+     * Return the admin users view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function getUsers(Request $request)
+    {
+        $table = new AdminUsers(User::query(), $request);
+
+        return view('mustard::admin.users', [
+            'table' => $table,
+        ]);
+    }
+
+    /**
+     * Return the admin mailout view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function getMailout()
+    {
+        return view('mustard::admin.mailout', [
+            'users' => User::orderBy('username', 'asc')->get(),
+        ]);
+    }
+
+    /**
+     * Send a mailout.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function postMailout(Request $request)
+    {
+        $this->validates(
+            $request->all(),
+            [
+                'users' => 'required',
+                'subject' => 'required|min:4',
+                'body' => 'required|min:10',
+            ]
+        );
+
+        $count = 0;
+
+        foreach (User::all() as $user) {
+            if (in_array($user->userId, $request->input('users'))) {
+                $user->sendEmail(
+                    $request->input('subject'),
+                    'emails.mailout',
+                    [
+                        'body' => $request->input('body'),
+                        'handle' => $user->getHandle(),
+                        'email' => $user->email,
+                        'joined' => $user->joined,
+                    ]
+                );
+
+                $count++;
+            }
+        }
+
+        return redirect()->back()->withMessage("Mailout sent to $count recipients.");
+    }
+}
