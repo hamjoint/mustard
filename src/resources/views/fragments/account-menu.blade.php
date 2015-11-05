81cb3644491eb52882b8cda0a1b8515df1a778f8
Tom Pitcher <tom@hamjoint.org>
2015-11-05 12:56:51 +0000

    Added conditional messages option to account menu

--- src/resources/views/fragments/account-menu.blade.php
+++ src/resources/views/fragments/account-menu.blade.php
@@ -2,7 +2,9 @@
 <ul class="dropdown">
     <li><a href="/inventory"><i class="fa fa-book"></i> Inventory</a></li>
     <li><a href="{{ Auth::user()->url }}"><i class="fa fa-user"></i> View profile</a></li>
-    <li><a href="/messages"><i class="fa fa-inbox"></i> Messages</a></li>
+    @if (mustard_loaded('messaging'))
+        <li><a href="/messages"><i class="fa fa-inbox"></i> Messages</a></li>
+    @endif
     <li><a href="/account"><i class="fa fa-wrench"></i> Account settings</a></li>
     <li><a href="/account/close"><i class="fa fa-eject"></i> Close account</a></li>
     <li><a href="/auth/logout"><i class="fa fa-sign-out"></i> Log out</a></li>
