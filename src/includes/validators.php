22746268852ff901846f6731747372a271473c57
Tom Pitcher <tom@hamjoint.org>
2015-11-17 21:13:49 +0000

    Added validators

--- src/includes/validators.php
+++ src/includes/validators.php
@@ -1,2 +1,30 @@
 <?php
 
+/*
+
+This file is part of Mustard.
+
+Mustard is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Mustard is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Mustard.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+Validator::extend('monetary', function($attribute, $value, $parameters)
+{
+    return (bool) preg_match('/^\d+(\.\d{2})?$/', $value);
+});
+
+Validator::extend('intrange', function($attribute, $value, $parameters)
+{
+    return (bool) preg_match('/^\d+(-\d+)?$/', $value);
+});
